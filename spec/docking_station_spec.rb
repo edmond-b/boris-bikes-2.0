723fdac7a6c290db82f72ccfe4e6d6a93da6e93b
Edmond <edmond.b@hotmail.co.uk>
2020-12-30 14:02:32 +0000

    passing first feature test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
   it 'responds to release_bike method' do
     expect(subject).to respond_to(:release_bike)
   end
+  it 'releases a working bike from station' do
+    bike = subject.release_bike
+    expect(bike.working?).to be(true)
+  end
 end
